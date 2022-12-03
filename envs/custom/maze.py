1e7a7f9437831543ce94cd195dd2b6c94e4eb97f
bayzhen <1521085156@qq.com>
2022-12-03 14:53:25 +0800

    Update maze.py

--- envs/custom/maze.py
+++ envs/custom/maze.py
@@ -7,10 +7,8 @@ import gym
 from gym import spaces
 from gym.utils import seeding, EzPickle
 
-FPS = 1
-SCALE = 30.0  # affects how fast-paced the game is, forces should be adjusted as well
-
-INITIAL_RANDOM = 1000.0  # Set 1500 to make game harder
+# FPS控制好像不管用
+FPS = 50
 
 VIEWPORT_W = 600
 VIEWPORT_H = 400
@@ -30,18 +28,53 @@ class Maze(gym.Env, EzPickle):
     maze_size_y = 10
     count = None
 
+    # 墙、路径、智能体的颜色
     wall_color = (0, 0, 1)
     path_color = (1, 1, 1)
     agent_color = (0, 1, 0)
 
+    # 智能体当前位置
     current_x = 1
     current_y = 1
+
+    # 目标位置
     destination_x = maze_size_x
     destination_y = maze_size_y
 
+    # step统计
     step_count = 0
+
+    # step上限
     step_up_limit = 2000
 
+    def __init__(self):
+        # EzPickle是读取和存储硬盘上的数据的包，没用，但可以留着。
+        EzPickle.__init__(self)
+
+        # 迷宫的数据存储在self.data中，存储方式如下两行。
+        self.data_sizeH, self.data_sizeW = self.maze_size_x * 2 + 1, self.maze_size_y * 2 + 1
+        self.data = np.zeros(shape=(self.data_sizeH, self.data_sizeW), dtype=int)
+
+        # 不明意义，无用暂时留下
+        self.seed()
+
+        # 用于渲染
+        self.viewer = None
+
+        # 前一个奖励，
+        self.prev_reward = None
+
+        # 状态空间
+        self.observation_space = spaces.Box(
+            -np.inf, np.inf, shape=(self.data_sizeH, self.data_sizeW), dtype=int
+        )
+        # 动作空间
+        self.action_space = spaces.Discrete(4)
+
+        # 重置函数
+        self.reset()
+
+    # 根据两个相邻的点，这两个点之间的边是否存在
     def get_edge(self, two_points):
         x0, y0, x1, y1 = two_points[0][0], two_points[0][1], two_points[1][0], two_points[1][1]
         data_x0 = 2 * x0 - 1
@@ -52,6 +85,7 @@ class Maze(gym.Env, EzPickle):
         data_y = int((data_y0 + data_y1) / 2)
         return self.data[data_x, data_y]
 
+    # 打破两个相邻点之间的边
     def break_edge(self, two_points):
         x0, y0, x1, y1 = two_points[0][0], two_points[0][1], two_points[1][0], two_points[1][1]
         data_x0 = 2 * x0 - 1
@@ -62,29 +96,19 @@ class Maze(gym.Env, EzPickle):
         data_y = int((data_y0 + data_y1) / 2)
         self.data[data_x, data_y] = 0
 
+    # 查看某一个位置的路径是否可走
     def get_point(self, point):
         x = point[0]
         y = point[1]
         return self.data[2 * x - 1][2 * y - 1]
 
+    # 设置某一个位置的路径是否可走
     def set_point(self, point, value):
         x = point[0]
         y = point[1]
         self.data[2 * x - 1][2 * y - 1] = value
 
-    def __init__(self):
-        EzPickle.__init__(self)
-        self.data_sizeH, self.data_sizeW = self.maze_size_x * 2 + 1, self.maze_size_y * 2 + 1
-        self.data = np.zeros(shape=(self.data_sizeH, self.data_sizeW), dtype=int)
-        self.seed()
-        self.viewer = None
-        self.prev_reward = None
-        self.observation_space = spaces.Box(
-            -np.inf, np.inf, shape=(self.data_sizeH, self.data_sizeW), dtype=int
-        )
-        self.action_space = spaces.Discrete(4)
-        self.reset()
-
+    # 重新生成迷宫
     def regenerate_maze_frame(self):
         self.data = np.zeros(shape=(self.data_sizeH, self.data_sizeW), dtype=int)
         i = 0
@@ -101,6 +125,7 @@ class Maze(gym.Env, EzPickle):
         self.data[:, self.maze_size_y * 2] = 2
         self.random_prime_create_maze()
 
+    # 迷宫生成算法
     def random_prime_create_maze(self):
         point_list = [[1, 1]]
         two_points_list = [[[1, 1], [1, 2]], [[1, 1], [2, 1]]]
@@ -128,10 +153,12 @@ class Maze(gym.Env, EzPickle):
                 two_points_list.append([out_point, left_point])
                 two_points_list.append([out_point, right_point])
 
+    # 遗留
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
+    # 重置
     def reset(self):
         self.regenerate_maze_frame()
         self.current_x = 1
@@ -139,6 +166,7 @@ class Maze(gym.Env, EzPickle):
         self.step_count = 0
         return self.step(0)[0]
 
+    # 单步
     def step(self, action):
         state = np.copy(self.data)
         reward = 0
@@ -178,14 +206,15 @@ class Maze(gym.Env, EzPickle):
             self.current_x, self.current_y = next_point[0], next_point[1]
             return np.array(state, dtype=int), reward, done, {}
 
+    # 渲染
     def render(self, mode="human"):
         from gym.envs.classic_control import rendering
         if self.viewer is None:
             self.viewer = rendering.Viewer(VIEWPORT_W, VIEWPORT_H)
-            self.viewer.set_bounds(0, VIEWPORT_W / SCALE, 0, VIEWPORT_H / SCALE)
+            self.viewer.set_bounds(0, VIEWPORT_W, 0, VIEWPORT_H)
 
-        box_w = VIEWPORT_W / (self.maze_size_x) / SCALE
-        box_h = VIEWPORT_H / (self.maze_size_y) / SCALE
+        box_w = VIEWPORT_W / (self.maze_size_x)
+        box_h = VIEWPORT_H / (self.maze_size_y)
         line_w = 5
         for i in range(1, self.maze_size_x + 1):
             for j in range(1, self.maze_size_y + 1):
@@ -196,15 +225,15 @@ class Maze(gym.Env, EzPickle):
                 p = [p0, p1, p2, p3]
                 self.viewer.draw_polygon(p, color=self.path_color)
         p0 = (0, 0)
-        p1 = (VIEWPORT_W / SCALE, 0)
-        p2 = (VIEWPORT_W / SCALE, 0)
+        p1 = (VIEWPORT_W, 0)
+        p2 = (VIEWPORT_W, 0)
         p3 = (0, 0)
         p = [p0, p1, p2, p3]
         self.viewer.draw_polyline(p, color=self.wall_color, linewidth=line_w)
 
         p0 = (0, 0)
-        p1 = (0, VIEWPORT_H / SCALE)
-        p2 = (0, VIEWPORT_H / SCALE)
+        p1 = (0, VIEWPORT_H)
+        p2 = (0, VIEWPORT_H)
         p3 = (0, 0)
         p = [p0, p1, p2, p3]
         self.viewer.draw_polyline(p, color=self.wall_color, linewidth=line_w)
@@ -231,23 +260,71 @@ class Maze(gym.Env, EzPickle):
 
         return self.viewer.render(return_rgb_array=mode == "rgb_array")
 
+    # 关闭环境
     def close(self):
         if self.viewer is not None:
             self.viewer.close()
             self.viewer = None
 
 
+# MazeSimple的尺寸默认为20*20，生成逻辑是固定的。
 class MazeSimple(Maze):
+    # count在Maze中默认是None，当count为确定int时，迷宫的生成逻辑将固定。
+    # 迷宫的生成逻辑为prime，当count固定，边缘的选择将固定，具体参考Maze.random_prime_create_maze()函数。
     count = 10
+    # 状态空间
+    observation_space = spaces.Box(
+        -np.inf, np.inf, shape=(1,2), dtype=int
+    )
+
+    # 单步
+    def step(self, action):
+        state = np.array([self.current_x, self.current_y]).astype(int)
+        reward = 0
+        done = False
+        if action == 0:
+            next_point = [self.current_x - 1, self.current_y]
+        elif action == 1:
+            next_point = [self.current_x + 1, self.current_y]
+        elif action == 2:
+            next_point = [self.current_x, self.current_y - 1]
+        elif action == 3:
+            next_point = [self.current_x, self.current_y + 1]
+        else:
+            assert self.action_space.contains(action), "%r (%s) invalid " % (
+                action,
+                type(action),
+            )
+        two_points = [[self.current_x, self.current_y], next_point]
+        self.step_count = self.step_count + 1
+        if self.step_count >= self.step_up_limit:
+            done = True
+            return np.array(state, dtype=int), reward, done, {}
+
+        if self.get_edge(two_points) == 1 or self.get_edge(two_points) == 2:
+            reward = -1
+            return np.array(state, dtype=int), reward, done, {}
+        else:
+            if next_point == [self.destination_x, self.destination_y]:
+                reward = 1
+                done = True
+            else:
+                reward = 0
+                done = False
+            self.current_x, self.current_y = next_point[0], next_point[1]
+            return np.array(state, dtype=int), reward, done, {}
 
 
-class MazeBigSimple(Maze):
+# MazeBigSimple的尺寸默认为40*40，
+class MazeBigSimple(MazeSimple):
+    # 同MazeSimple
     count = 10
-    maze_size_x = 20
-    maze_size_y = 20
+    # override迷宫尺寸
+    maze_size_x = 40
+    maze_size_y = 40
 
-
-def demo_heuristic_maze(env, seed=None, render=False):
+# 测试函数
+def demo_maze(env, seed=None, render=False):
     env.seed(seed)
     total_reward = 0
     steps = 0
@@ -255,6 +332,7 @@ def demo_heuristic_maze(env, seed=None, render=False):
     while True:
         a = random.choice([0, 1, 2, 3])
         s, r, done, info = env.step(a)
+        print(s,r,done)
         total_reward += r
 
         if render:
@@ -271,4 +349,4 @@ def demo_heuristic_maze(env, seed=None, render=False):
 
 
 if __name__ == "__main__":
-    demo_heuristic_maze(MazeBigSimple(), render=True)
+    demo_maze(Maze(), render=True)
